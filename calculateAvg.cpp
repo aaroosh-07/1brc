16973366ebf44b99381bec190b876752ceb23386
Aaroosh <aaroosh.2012@gmail.com>
2026-05-10 18:02:34 +0530

    Optimized file I/O

--- calculateAvg.cpp
+++ calculateAvg.cpp
@@ -5,6 +5,78 @@
 #include<vector>
 #include<algorithm>
 #include<iomanip>
+#include<filesystem>
+#include<sys/mman.h>
+#include<fcntl.h>
+#include<unistd.h>
+#include<sys/stat.h>
+#include<sys/types.h>
+#include<charconv>
+
+class FileDes
+{
+    int fileFd = -1;
+public:
+    explicit FileDes(std::filesystem::path& filePath)
+    {
+        fileFd = open(filePath.c_str(), O_RDONLY);
+
+        if (fileFd < 0)
+            throw std::system_error(errno, std::system_category(), "Unable to open File");
+    }
+
+    ~ FileDes()
+    {
+        if (fileFd >= 0)
+            close(fileFd);
+    }
+
+    const int getFileFd()
+    {
+        return fileFd;
+    }
+};
+
+class MemMapFile
+{
+    FileDes fileDes;
+    char* buffer = nullptr;
+    std::size_t size = 0;
+
+public:
+    explicit MemMapFile(std::filesystem::path filePath) : fileDes(filePath)
+    {
+        struct stat fs;
+        if (fstat(fileDes.getFileFd(), &fs) == 1)
+            throw std::system_error(errno, std::system_category(), "Unable to read file stats");
+
+        size = fs.st_size;
+        buffer = static_cast<char*>(mmap(NULL, size, PROT_READ, MAP_PRIVATE, fileDes.getFileFd(), 0));
+
+        if (buffer == MAP_FAILED)
+        {
+            throw std::system_error(errno, std::system_category(), "Unable to map file");
+        }
+    }
+
+    ~ MemMapFile()
+    {
+        if (buffer != nullptr)
+        {
+            munmap(buffer, size);
+        }
+    }
+
+    const char* getBuffer()
+    {
+        return buffer;
+    }
+
+    std::size_t getSize()
+    {
+        return size;
+    }
+};
 
 struct Record
 {
@@ -14,9 +86,74 @@ struct Record
     float minValue = 0.0;
 };
 
-std::unordered_map<std::string, Record> processInput(std::ifstream& inputFile)
+struct StringHash
 {
-    std::unordered_map<std::string, Record> cityRecord;
+    using is_transparent = void;
+
+    size_t operator()(std::string_view txt) const
+    {
+        return std::hash<std::string_view>{}(txt);
+    }
+
+    size_t operator()(const std::string& txt) const
+    {
+        return std::hash<std::string>{}(txt);
+    }
+};
+
+using hashmap = std::unordered_map<std::string, Record, StringHash, std::equal_to<>>;
+
+hashmap processInput(const char* bufferStart, std::size_t size)
+{
+    hashmap cityRecord;
+
+    const char* bufferEnd = bufferStart + size;
+    const char* curPointer = bufferStart;
+
+    while (curPointer < bufferEnd)
+    {
+        const char* cityStartPtr = curPointer;
+        while (*curPointer != ';')
+        {
+            curPointer++;
+        }
+
+        std::string_view city(cityStartPtr, curPointer - cityStartPtr);
+
+        // skip ; character
+        curPointer++;
+
+        const char* valueStartPtr = curPointer;
+        while (*curPointer != '\n')
+        {
+            curPointer++;
+        }
+
+        float value;
+        std::from_chars(valueStartPtr, curPointer, value);
+        curPointer++;
+
+        auto itr = cityRecord.find(city);
+        if (itr == cityRecord.end())
+        {
+            cityRecord.emplace(city, Record{1, value, value, value});
+        }
+        else
+        {
+            Record& record = itr->second;
+            record.count++;
+            record.sum += value;
+            record.maxValue = std::max(record.maxValue, value);
+            record.minValue = std::min(record.minValue, value);
+        }
+    }
+
+    return cityRecord;
+}
+
+hashmap processInput(std::ifstream& inputFile)
+{
+    hashmap cityRecord;
     std::string city, value;
     while(std::getline(inputFile, city, ';') && std::getline(inputFile, value, '\n'))
     {
@@ -38,7 +175,7 @@ std::unordered_map<std::string, Record> processInput(std::ifstream& inputFile)
     return cityRecord;
 }
 
-void processOutput(std::ostream& outputStream, std::unordered_map<std::string, Record>& cityRecords)
+void processOutput(std::ostream& outputStream, hashmap& cityRecords)
 {
     std::vector<std::string> cities;
     cities.reserve(cityRecords.size());
@@ -75,9 +212,20 @@ void bruteForceSol()
     processOutput(std::cout, cityRecords);
 }
 
+void improvedSolWithMmapFiles()
+{
+    MemMapFile memMappedFile(std::filesystem::path("data/measurements.txt"));
+    auto cityRecords = processInput(memMappedFile.getBuffer(), memMappedFile.getSize());
+    processOutput(std::cout, cityRecords);
+}
+
 int main()
 {
+    // Call this fn for BruteForce sol
     bruteForceSol();
+
+    // Improved Implementation with Mem Mapped files only
+    // improvedSolWithMmapFiles();
 
     return 0;
 }
